afd0e3dbe4fb9aa3ab00675ebc6e10d56b0a1c0b
james-moreno <bonsaimo7@gmail.com>
2017-01-20 22:24:02 -0800

    discarded tiles now visible. tiles are tile shaped. clicking a tile will discard it.

--- client/assets/app.js
+++ client/assets/app.js
@@ -1,8 +1,8 @@
 var app = angular.module('app', []);
 
 app.controller('gameController', ['$scope',  function($scope){
-    var tableDiscards = [];
-    var tableDiscard;
+    $scope.tableDiscards = [];
+    $scope.tableDiscard = undefined;
     $scope.players = [];
     var turn = 0;
 
@@ -23,8 +23,9 @@ app.controller('gameController', ['$scope',  function($scope){
     }
 
     function newDiscard(tile){
-        tableDiscards.push(tableDiscard);
-        tableDiscard = tile;
+        console.log(tile);
+        $scope.tableDiscards.push($scope.tableDiscard);
+        $scope.tableDiscard = tile;
     }
 
 
@@ -41,7 +42,7 @@ app.controller('gameController', ['$scope',  function($scope){
         for(var j = 1; j <= 4; j++){
             for(var i = 1; i <= 9; i++){
                 this.wall[this.wall.length] = new Tile('bamboo', i);
-                this.wall[this.wall.length] = new Tile('spot', i);
+                this.wall[this.wall.length] = new Tile('aspot', i);
                 this.wall[this.wall.length] = new Tile('char', i);
             }
             this.wall[this.wall.length] = new Tile("north", null);
@@ -91,12 +92,11 @@ app.controller('gameController', ['$scope',  function($scope){
     Player.prototype.drawTile = function(){
         this.hand.push(newWall.wall.pop());
     };
-    Player.prototype.discard = function(){
-        var discard = this.hand.pop();
+    Player.prototype.discard = function(index){
+        var discard = this.hand.splice(index, 1);
         if(this.hand.length){
-            newDiscard(discard);
+            newDiscard(discard[0]);
         }
-        console.log(this.hand);
     };
     Player.prototype.sortBy = function (key, minor) {
     return function (o, p) {
@@ -114,6 +114,9 @@ app.controller('gameController', ['$scope',  function($scope){
             }
         };
     };
+    Player.prototype.sortHand = function(){
+        this.hand.sort(this.sortBy('suit', this.sortBy('value')));
+    };
 
     //Tests
     var playerOne = new Player("1");
@@ -126,6 +129,7 @@ app.controller('gameController', ['$scope',  function($scope){
     var newWall = new Wall();
     newWall.shuffle();
     newWall.dealTiles();
+    $scope.players[0].sortHand();
     }
 ]);
 
